8aa7097a210d28de794e1a0bc0af76478ccbef64
skaufman <sam@socialflow.com>
2013-01-21 02:16:50 +0000

    closer to the metal, using Net::OAuth2

--- lib/pdd/Auth/Google.pm
+++ lib/pdd/Auth/Google.pm
@@ -1,62 +1,58 @@
 package pdd::Auth::Google;
 use strictures 1;
-use Moo;
+use pdd::Moose;
 use Net::Google::FederatedLogin;
 use Function::Parameters ':strict';
+use Net::OAuth2::Profile::WebServer;
+use URI;
+use Data::Dumper::Concise;
+use 5.14.0;
 my $class = __PACKAGE__;
 
-has return_to => ( 
+has _auth => ( 
+    is => 'lazy', 
+    handles => {
+        ('authorize')x2,
+        ('get_access_token')x2,
+    }
+);
+
+has config => ( is => 'lazy', isa => HashRef );
+
+has scope => ( is => 'ro', required => 1, isa => ArrayRef );
+
+has redirect_uri => (
     is => 'ro',
     required => 1,
 );
+sub _build_config {
+    require pdd::Config or die "$@ $!";
+    my $cfg = pdd::Config->config;
+    return $cfg->{google_oauth2}{web};
+}
 
-has realm => (
-    is => 'ro',
-    required => 1,
-);
+method _build__auth() {
+    my $config = $self->config;
 
-has google_federated_login => (
-    is        => 'lazy',
-    handles => { get_auth_url => 'get_auth_url' },
-);
-
-sub _build_google_federated_login {
-    my $self = shift;
-    Net::Google::FederatedLogin->new(
-        claimed_id => "gmail.com",
-        return_to  => sprintf( "%s" , $self->return_to ),
-        realm      => sprintf( "%s" , $self->realm ),
-        extensions => {
-            'http://specs.openid.net/extensions/ui/1.0' =>
-              Net::Google::FederatedLogin::Extension->new(
-                ns         => 'ui',
-                uri        => 'http://specs.openid.net/extensions/ui/1.0',
-                attributes => { icon => 'true' }
-              ),
-            'http://openid.net/srv/ax/1.0' =>
-              Net::Google::FederatedLogin::Extension->new(
-                ns         => 'ax',
-                uri        => 'http://openid.net/srv/ax/1.0',
-                attributes => {
-                    mode         => 'fetch_request',
-                    required     => 'email',
-                    'type.email' => 'http://axschema.org/contact/email'
-                }
-           )
-        }
+    my %p      = (
+        client_id     => $config->{client_id},
+        client_secret => $config->{client_secret},
+        redirect_uri => $self->redirect_uri,
+        auto_refresh => 1,
     );
-}
 
-method verify($cgi_params) {
-    return 0 unless Net::Google::FederatedLogin->new(
-        cgi_params => $cgi_params,
-        return_to => sprintf( "%s" , $self->return_to ),
-    )->verify_auth;
-
-    return {
-        email => $cgi_params->{'openid.ext1.value.email'},
-    };
-}
+    my $u = URI->new( $config->{auth_uri} );
+    warn Dumper [ $u->scheme, $u->host, $u->port ];
+    $p{site} = sprintf(
+        "%s" => URI->new( $u->scheme . "://" . $u->host . ":" . $u->port )
+          ->canonical );
+    $p{authorize_path} = sprintf( "%s" => $u->path );
+    $p{scope} = join " " => @{ $self->scope };
+    $p{access_token_path} =
+      sprintf( "%s" => URI->new( $config->{token_uri} )->path );
+    my $auth = Net::OAuth2::Profile::WebServer->new(%p);
+ }
 
 $class->meta->make_immutable;
+
 1;
